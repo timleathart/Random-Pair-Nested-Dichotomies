d89cc0b96c214bc941fc882336179144c7ed2404
Tim Leathart <timleathart@gmail.com>
2017-03-20 14:37:49 +1300

    added proper python package structure, added ability to train ND from specific structure

---
+++ python/nested_dichotomy/__init__.py
@@ -0,0 +1,1 @@
+from nested_dichotomy import NestedDichotomy
--- python/nested_dichotomy/nested_dichotomy.py
+++ python/nested_dichotomy/nested_dichotomy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import collections
 
 from sklearn.base import BaseEstimator, ClassifierMixin, MetaEstimatorMixin
 from sklearn.base import clone, is_classifier
@@ -10,6 +11,12 @@ subset_selection_methods = [
     "class_balanced",
     "random_pair"
 ]
+
+def flatten_nested_tuple(x):
+    if isinstance(x, collections.Iterable):
+        return [a for i in x for a in flatten_nested_tuple(i)]
+    else:
+        return [x]
 
 
 class NestedDichotomy(BaseEstimator, ClassifierMixin):
@@ -27,8 +34,9 @@ class NestedDichotomy(BaseEstimator, ClassifierMixin):
         self.estimator = estimator
         self.subset_selection_method = subset_selection_method
         self.parent = parent
+        self.structure = None
 
-    def fit(self, X, y):
+    def fit(self, X, y, structure=None):
         labels = np.unique(y)
 
         # Set classes attribute
@@ -40,21 +48,27 @@ class NestedDichotomy(BaseEstimator, ClassifierMixin):
         # Create leaf node if only one class present
         if len(labels) == 1:
             self.estimator = _ConstantPredictor().fit(X, labels)
+            self.structure = (labels[0],)
             return
 
-        # Create split node if more than one class present
-        pos_labels, neg_labels = None, None
+        if structure is None:
+            # Create split node if more than one class present
+            pos_labels, neg_labels = None, None
+            structure = (None, None)
 
-        if len(labels) == 2:
-            pos_labels, neg_labels = np.array_split(labels, 2)
+            if len(labels) == 2:
+                pos_labels, neg_labels = np.array_split(labels, 2)
+            else:
+                if self.subset_selection_method == "random":
+                    pos_labels, neg_labels = self._random_selection(labels)
+                elif self.subset_selection_method == "class_balanced":
+                    pos_labels, neg_labels = self._class_balanced(labels)
+                elif self.subset_selection_method == "random_pair":
+                    pos_labels, neg_labels = self._random_pair_selection(
+                        labels, X, y)
         else:
-            if self.subset_selection_method == "random":
-                pos_labels, neg_labels = self._random_selection(labels)
-            elif self.subset_selection_method == "class_balanced":
-                pos_labels, neg_labels = self._class_balanced(labels)
-            elif self.subset_selection_method == "random_pair":
-                pos_labels, neg_labels = self._random_pair_selection(
-                    labels, X, y)
+            pos_labels = flatten_nested_tuple(structure[0])
+            neg_labels = flatten_nested_tuple(structure[1])
 
         # Generate new labels
         new_y = np.array(map(lambda y_val: 1 if y_val in pos_labels else 0, y))
@@ -77,8 +91,11 @@ class NestedDichotomy(BaseEstimator, ClassifierMixin):
         pos_X, pos_y = X[new_y == 1], y[new_y == 1]
         neg_X, neg_y = X[new_y == 0], y[new_y == 0]
 
-        self.left.fit(pos_X, pos_y)
-        self.right.fit(neg_X, neg_y)
+        self.left.fit(pos_X, pos_y, structure=structure[0])
+        self.right.fit(neg_X, neg_y, structure=structure[1])
+
+        # make sure structure is updated
+        self.structure = (self.left.structure, self.right.structure)
 
     def predict(self, X):
         preds = self.estimator.predict(X)
@@ -149,17 +166,10 @@ class NestedDichotomy(BaseEstimator, ClassifierMixin):
         for y_val in labels:
             if y_val in labels[:2]:
                 continue
-                
+
             if cm[y_val][0] < cm[y_val][1]:
-                pos_labels += [y_val]
+                pos_labels.append(y_val)
             else:
-                neg_labels += [y_val]      
-        
+                neg_labels.append(y_val)
+
         return pos_labels, neg_labels
-
-
-
-
-
-
-
